6d9c0249c12c173c5b42fcd0c5c7d3bcd2687d93
ebaker15 <ebaker@sjnma.org>
2017-07-10 10:45:13 -0400

    Commit
    Added

---
+++ p0kem0n.py
@@ -0,0 +1,35 @@
+
+
+
+print(Oak       : Hello there! Welcome to the world of POKEMON! My name is OAK!
+            People call me the POKEMON PROF!)
+
+
+
+print(Oak       : This world is inhabited by creatures called POKEMON! For some
+            people, POKEMON are pets. Others use them for fights. Myself...
+            I study POKEMON as a profession.)
+
+
+
+print(Oak       : First, what is your name?)
+input(Please Enter Your Name.)
+
+* - The first option is that of the version which one is playing.
+
+print(Oak       : Right! So your name is (input))
+
+
+
+print(Oak       : This is my grandson. He's been your rival since you were a baby.)
+
+print(Oak       :            ...Erm, what is his name again?)
+
+* - The first option is that of the version which one is not playing.
+
+print(Oak       : That's right! I remember now! His name is Autism.)
+
+* - Red's image reappears.
+
+print(Oak       : RED! Your very own POKEMON legend is about to unfold! A world of
+            dreams and adventures with POKEMON awaits! Let's go!)
