a6007b2e0899355b6acabd3b949132a28b305eb9
ebaker15 <ebaker@sjnma.org>
2017-07-10 12:18:28 -0400

    Commit
    Add to github

---
+++ pokemon.py
@@ -0,0 +1,33 @@
+
+
+
+print("Oak       :Hello there! Welcome to the world of POKEMON! My name is OAK!")
+print("Oak       :People call me the POKEMON PROF!")
+
+
+
+print("Oak       :This world is inhabited by creatures called POKEMON! For somepeople, POKEMON are pets. Others use them for fights. Myself...I study POKEMON as a profession.")
+
+
+
+print("Oak       :First, what is your name?")
+(trainer = input("Please Enter Your Name"))
+
+print(trainer)
+
+
+
+print("Oak       : Right! So your name is" print(trainer)
+
+
+print("Oak       : This is my grandson. He's been your rival since you were a baby.")
+
+print("Oak       :            ...Erm, what is his name again?")
+
+
+
+print("Oak       : That's right! I remember now! His name is Autism.")
+
+
+
+print("Oak       : RED! Your very own POKEMON legend is about to unfold! A world of dreams and adventures with POKEMON awaits! Let's go!")
